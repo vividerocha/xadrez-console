977eee34cfecde8a03d076dc25b44b91ed8f4791
VIVIANE OLIVEIRA ROCHA <56705207+vividerocha@users.noreply.github.com>
2020-04-30 19:19:18 -0300

    Implementação do Tabuleiro

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Tabuleiro tabuleiro, Cor cor)
+        {
+            this.posicao = null;
+            this.tab = tabuleiro;
+            this.cor = cor;
+            this.qtdMovimentos = 0;
+        }
+
+       
+    }
+}
